5bf828b6e054f0f2724c77107cd5478ddae19187
GWeightman <g.weightman@hotmail.co.uk>
2021-12-16 16:28:54 +0000

    DELETE function added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 require("./db/connections")
 const yargs = require("yargs")
-const { addMovie } = require("./movie/movie.functions")
+const mongoose = require("mongoose")
+const { addMovie, deleteMovie } = require("./movie/movie.functions")
 
 const app = async (args) => {
     try {
         if (args.add) {
             const movieObj = {title: args.title, actor: args.actor, rating: args.rating}
             await addMovie(movieObj)
+        }
+        else if (args.delete){
+            const movieObj = {title: args.title}
+            await deleteMovie(movieObj)
         }
         else{
             console.log("Incorrect command")
--- src/movie/movie.functions.js
+++ src/movie/movie.functions.js
@@ -5,6 +5,18 @@ exports.addMovie = async (movieObj) => {
     try{
         const newMovie = new Movie(movieObj)
         await newMovie.save()
+        console.log(`Successfully added ${movieObj.title}`)
+        mongoose.disconnect()
+    } catch (error) {
+        console.log (error)
+        mongoose.disconnect()
+    }
+}
+
+exports.deleteMovie = async (movieObj) => {
+    try{
+        await Movie.deleteOne(movieObj)
+        console.log(`Successfully deleted ${movieObj.title}`)
         mongoose.disconnect()
     } catch (error) {
         console.log (error)
